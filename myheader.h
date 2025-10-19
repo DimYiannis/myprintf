e3403e57864e98a30ff76f811d7bec142efa3850
Yiannis Dimitrakopoulos <ydimitra@f0r6s11.codam.nl>
2025-10-19 16:19:48 +0200

    prototypes

--- myheader.h
+++ myheader.h
@@ -6,7 +6,18 @@
 /*   By: ydimitra <ydimitra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/19 11:06:46 by ydimitra          #+#    #+#             */
-/*   Updated: 2025/10/19 11:06:47 by ydimitra         ###   ########.fr       */
+/*   Updated: 2025/10/19 16:19:24 by ydimitra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef FT_MYHEADER_H
+# define FT_MYHEADER_H
+# include <stdio.h>
+# include <stdlib.h>
+#include <unistd.h>
+
+unsigned int	count_digits(unsigned int n);
+unsigned			*ft_unsigneditoa(unsigned int n);
+void			ft_hexputnbr(int n, int upper);
+
+#endif
