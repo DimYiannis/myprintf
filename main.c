bc054b6d617909a800c752643324fc59209267e1
DimYiannis <johndim99@gmail.com>
2025-10-20 12:36:57 +0200

    checking for pointer

--- main.c
+++ main.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include "myheader.h"
 
 int main()
 {
     char *p;
     char c = 'l';
     p = &c;
-    ft_printf("%p", p);
-    printf("%p", p);
+    ft_printf("wanabee: %p\n", p);
+    printf("legit one: %p\n", p);
 }
