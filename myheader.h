b9e318edb7a81774c465bb9c0b1a636e12bef04b
Yiannis Dimitrakopoulos <ydimitra@f0r6s11.codam.nl>
2025-10-20 10:49:35 +0200

    fixed prototypes maybe i will use the stdint lib

--- myheader.h
+++ myheader.h
@@ -6,7 +6,7 @@
 /*   By: ydimitra <ydimitra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/19 11:06:46 by ydimitra          #+#    #+#             */
-/*   Updated: 2025/10/19 21:29:12 by ydimitra         ###   ########.fr       */
+/*   Updated: 2025/10/20 10:48:17 by ydimitra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,12 @@
 # include <stdio.h>
 # include <stdlib.h>
 #include <unistd.h>
+#include <stdint.h>
 #include "libft/libft.h"
 
 unsigned int	count_digits(unsigned int n);
 char			*ft_unsigneditoa(unsigned int n);
-void			ft_hexputnbr(int n, int upper);
+void			ft_hexputnbr(unsigned int n, int upper);
+int	ft_printf(const char *type, ...);
 
 #endif
