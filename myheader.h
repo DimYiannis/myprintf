798aa8e03a4b4582eef168e7de70eec760e8635d
DimYiannis <johndim99@gmail.com>
2025-10-20 12:34:27 +0200

    i need to use the address as a number so i am going to use another functoin similar for hexputnbr

--- myheader.h
+++ myheader.h
@@ -6,7 +6,7 @@
 /*   By: ydimitra <ydimitra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/19 11:06:46 by ydimitra          #+#    #+#             */
-/*   Updated: 2025/10/20 10:48:17 by ydimitra         ###   ########.fr       */
+/*   Updated: 2025/10/20 12:33:35 by ydimitra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,7 @@
 unsigned int	count_digits(unsigned int n);
 char			*ft_unsigneditoa(unsigned int n);
 void			ft_hexputnbr(unsigned int n, int upper);
+void	ft_addressputnbr(uintptr_t n);
 int	ft_printf(const char *type, ...);
 
 #endif
