c23b37aff2007885350b72f8aa561ba8995a5d16
Yiannis Dimitrakopoulos <ydimitra@f0r2s15.codam.nl>
2025-10-22 14:30:07 +0200

    check conversions

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: ydimitra <ydimitra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/19 11:06:46 by ydimitra          #+#    #+#             */
-/*   Updated: 2025/10/22 08:31:50 by ydimitra         ###   ########.fr       */
+/*   Updated: 2025/10/22 14:25:28 by ydimitra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,5 +24,6 @@ int				ft_hexputnbr(unsigned int n, int upper);
 int				ft_addressputnbr(void *p);
 uintptr_t		address_len(uintptr_t n);
 int				ft_printf(const char *type, ...);
+void	check_convertion(const void *type, int n);
 
 #endif
