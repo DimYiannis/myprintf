12c40c4aec483c128bc964b8de4855eea308f2f9
Yiannis Dimitrakopoulos <ydimitra@f0r2s15.codam.nl>
2025-10-22 16:31:32 +0200

    new prototype

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: ydimitra <ydimitra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/19 11:06:46 by ydimitra          #+#    #+#             */
-/*   Updated: 2025/10/22 14:25:28 by ydimitra         ###   ########.fr       */
+/*   Updated: 2025/10/22 16:30:59 by ydimitra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,7 @@ unsigned int	count_digits(unsigned int n);
 char			*ft_unsigneditoa(unsigned int n);
 int				ft_hexputnbr(unsigned int n, int upper);
 int				ft_addressputnbr(void *p);
-uintptr_t		address_len(uintptr_t n);
+int				printing_int(char c, int num);
 int				ft_printf(const char *type, ...);
-void	check_convertion(const void *type, int n);
 
 #endif
