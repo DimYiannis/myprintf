9d6579ae1cfd3a347bacdaa3ecb143fdd3ef6436
DimYiannis <johndim99@gmail.com>
2025-10-22 08:33:56 +0200

    norm

--- ft_printf.h
+++ ft_printf.h
@@ -6,12 +6,12 @@
 /*   By: ydimitra <ydimitra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/19 11:06:46 by ydimitra          #+#    #+#             */
-/*   Updated: 2025/10/21 20:43:10 by ydimitra         ###   ########.fr       */
+/*   Updated: 2025/10/22 08:31:50 by ydimitra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MYHEADER_H
-# define MYHEADER_H
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
 # include "libft/libft.h"
 # include <stdint.h>
 # include <stdio.h>
@@ -20,8 +20,8 @@
 
 unsigned int	count_digits(unsigned int n);
 char			*ft_unsigneditoa(unsigned int n);
-int             ft_hexputnbr(unsigned int n, int upper);
-int	            ft_addressputnbr(void *p);
+int				ft_hexputnbr(unsigned int n, int upper);
+int				ft_addressputnbr(void *p);
 uintptr_t		address_len(uintptr_t n);
 int				ft_printf(const char *type, ...);
 
