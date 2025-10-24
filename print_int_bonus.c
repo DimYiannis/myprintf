82fc80f1efdb1e96eac478f4fafff2eaa3558d81
DimYiannis <johndim99@gmail.com>
2025-10-25 00:08:56 +0300

    logic for int still need to check for other occassions

--- print_int_bonus.c
+++ print_int_bonus.c
@@ -6,13 +6,13 @@
 /*   By: ydimitra <ydimitra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/24 09:54:44 by ydimitra          #+#    #+#             */
-/*   Updated: 2025/10/24 11:20:06 by ydimitra         ###   ########.fr       */
+/*   Updated: 2025/10/25 00:05:28 by ydimitra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	int_case(va_list *args, char c)
+int	int_case(t_print *tab, char c)
 {
 	char	*tmp;
 	int		len;
@@ -21,14 +21,94 @@ int	int_case(va_list *args, char c)
 
     if (c == "i" || c == "d")
 	{
-		num = va_arg(*args, int);
+		num = va_arg(tab->args, int);
 		tmp = ft_itoa(num);
 		if (!tmp)
 			return (0);
 		len = ft_strlen(tmp);
+		if (tab->width)
+		{
+			if (len < tab->width)
+			{
+				len = write(1, " ", tab->width - len);
+				len += write(1, &tmp, len);
+			}
+			else
+				len = write(1, &tmp, len);
+		}
+		else if (tab->width && tab->sign)
+		{
+			if (num <= 0)
+			{
+				if (len < tab->width)
+				{
+					len = write(1, " ", tab->width - len);
+					len += write(1, &tmp, len);
+				}
+				else
+					len = write(1, &tmp, len);
+			}
+			else
+			{
+				if (len < tab->width)
+				{
+					len = write(1, " ", tab->width - len - 1);
+					len += write(1, "+", 1);
+					len += write(1, &tmp, len);
+				}
+				else
+					len = write(1, &tmp, len);
+			}
+		}
+		else if (tab->sp)
+		{
+			len = write(1, " ", 1);
+			len += write(1, &tmp, len);
+			if (tab->width)
+			{
+				if (len > tab->width)
+					len = write(1, tmp, len);
+				else
+				{
+					len = write(1, " ", tab->width - len);
+					len += write(1, &tmp, len)
+				}
+			}
+		}
+		else if (tab->width && tab->zero)
+    	{
+        	write(1, "0", tab->width - len);
+			write(1, tmp, len);
+    	}
+		else if (tab->width)
+		{
+			if (len < tab->width)
+			{
+				if (tab->precision)
+				{
+					len = write(1, " ", tab->width - tab->precision);
+					if (len < tab->precision)
+					{
+						len += write(1, "0", tab->precision - len);
+						len += write(1, s, len);
+					}
+					else
+						len += write(1, s, len);
+				}
+				else
+				{
+					len = write(1," ",tab->width - len)
+					len += write(1, s, len);
+				}	
+				return (len);
+			}
+		}
+		else if (tab->dash)
+		{
+			
+		}
 		write(1, tmp, len);
 		free(tmp);
 		return (len);
 	}
-	return (0);
 }
