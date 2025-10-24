01cd5d987f2d1a7e7e444a5869b7c396589d1fa2
DimYiannis <johndim99@gmail.com>
2025-10-24 22:11:43 +0300

    fixed small things

--- print_char_bonus.c
+++ print_char_bonus.c
@@ -6,7 +6,7 @@
 /*   By: ydimitra <ydimitra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/24 09:55:33 by ydimitra          #+#    #+#             */
-/*   Updated: 2025/10/24 20:39:17 by ydimitra         ###   ########.fr       */
+/*   Updated: 2025/10/24 22:11:18 by ydimitra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,15 @@
 
 static int	char_case(t_print *tab, char c)
 {
-	char	*tmp;
-	void	*ptr;
-	char	ch;
-
-	if (c == 'c')
+	if (tab->dash && tab->width)
 	{
-		if (tab->dash && tab->width)
-		{
-			write(1, &c, 1);
-			write(1, " ", tab->width);
-		}
-		else if (tab->width)
-		{
-			write(1, " ", tab->width);
-			write(1, &c, 1);
-		}
-		return (tab->width + 1);
+		write(1, &c, 1);
+		write(1, " ", tab->width);
 	}
-	return (0);
+	else if (tab->width)
+	{
+		write(1, " ", tab->width);
+		write(1, &c, 1);
+	}
+	return (tab->width + 1);
 }
