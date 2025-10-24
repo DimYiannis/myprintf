bb89e251f26e9e681f68f6ca0146635e3a6d6ab0
Yiannis Dimitrakopoulos <ydimitra@f0r2s15.codam.nl>
2025-10-24 14:19:02 +0200

    new func that spots the type of conversion and proceeds with the nessecary func to print and count

--- eval_format_bonus.c
+++ eval_format_bonus.c
@@ -6,15 +6,15 @@
 /*   By: ydimitra <ydimitra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/24 12:14:29 by ydimitra          #+#    #+#             */
-/*   Updated: 2025/10/24 12:25:07 by ydimitra         ###   ########.fr       */
+/*   Updated: 2025/10/24 14:14:14 by ydimitra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf_bonus.h"
 
-void	eval_format(char *c, t_print *tab)
+unsigned int	eval_format(char *c, t_print *tab)
 {
-	int	i;
+	unsigned int	i;
 
 	i = 0;
 	while (!ft_isalpha(c[i]))
@@ -33,6 +33,27 @@ void	eval_format(char *c, t_print *tab)
 		{
 			tab->zero = 1;
 			i++;
-		}
+		}	
 	}
+	return (i);
+}
+
+void decide_conv(char *c, t_print *tab)
+{
+	unsigned int i
+
+	i == 0;
+	if (*c == 'd' || *c == 'i')
+		i += int_case(args, *c);
+	else if (*c == 'c')
+		i += char_case(args, *c)
+	else if (*c == 's')
+		i += string_case(args, *c);
+	else if (*c == 'x' || *c == 'X')
+		i += hex_case(args, *c);
+	else if (*c == 'p')
+		i += pointer_case(va_list *args, char c);
+	else if (*c == 'u')
+		i += unsigned_case(va_list *args, char c);
+	return (i);
 }
