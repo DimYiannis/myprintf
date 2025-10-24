79d57a7cf6c7f155da5126a8388ed225f0f27e63
DimYiannis <johndim99@gmail.com>
2025-10-24 20:38:26 +0300

    didnt need to access the next arg

--- print_char_bonus.c
+++ print_char_bonus.c
@@ -6,7 +6,7 @@
 /*   By: ydimitra <ydimitra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/24 09:55:33 by ydimitra          #+#    #+#             */
-/*   Updated: 2025/10/24 20:33:53 by ydimitra         ###   ########.fr       */
+/*   Updated: 2025/10/24 20:37:48 by ydimitra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,16 +20,15 @@ static int	char_case(va_list *args, char c)
 
 	if (c == 'c')
 	{
-		ch = (char)va_arg(*args, int);
 		if (tab->dash && tab->width)
 		{
-			write(1, &ch, 1);
+			write(1, &c, 1);
 			write(1, " ", tab->width);
 		}
 		else if (tab->width)
 		{
 			write(1, " ", tab->width);
-			write(1, &ch, 1);
+			write(1, &c, 1);
 		}
 		return (tab->width + 1);
 	}
