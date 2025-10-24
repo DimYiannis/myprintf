b9a6f16a91416ae4e86f837d54faca93cd7b134f
DimYiannis <johndim99@gmail.com>
2025-10-24 14:19:39 +0200

    small changes in the prototypes

--- ft_printf_bonus.h
+++ ft_printf_bonus.h
@@ -6,7 +6,7 @@
 /*   By: ydimitra <ydimitra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/24 11:17:48 by ydimitra          #+#    #+#             */
-/*   Updated: 2025/10/24 11:36:16 by ydimitra         ###   ########.fr       */
+/*   Updated: 2025/10/24 14:12:04 by ydimitra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,10 +39,10 @@ int				ft_hexputnbr(unsigned int n, int upper);
 int				ft_addressputnbr(void *p);
 uintptr_t		address_len(uintptr_t n);
 int				ft_printf(const char *type, ...);
-int				rest_cases(char c);
+int				char_case(char c);
 int				string_case(va_list *args, char c);
 int				pointer_case(va_list *args, char c);
-int				hex_case(va_list *args, const char *c, size_t *tranversed);
+int				hex_case(va_list *args, const char *c);
 int				int_case(va_list *args, char c);
 static int		unsigned_case(va_list *args, char c);
 int				ft_printf_bonus(const char *type, ...);
