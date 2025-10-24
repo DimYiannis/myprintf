1fbdef7a37a11ca161f9e793c7692ae129ca6c2f
DimYiannis <johndim99@gmail.com>
2025-10-24 20:34:50 +0300

    logic for handling the flags

--- print_char_bonus.c
+++ print_char_bonus.c
@@ -6,7 +6,7 @@
 /*   By: ydimitra <ydimitra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/24 09:55:33 by ydimitra          #+#    #+#             */
-/*   Updated: 2025/10/24 14:10:07 by ydimitra         ###   ########.fr       */
+/*   Updated: 2025/10/24 20:33:53 by ydimitra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,17 @@ static int	char_case(va_list *args, char c)
 	if (c == 'c')
 	{
 		ch = (char)va_arg(*args, int);
-		return (write(1, &ch, 1));
+		if (tab->dash && tab->width)
+		{
+			write(1, &ch, 1);
+			write(1, " ", tab->width);
+		}
+		else if (tab->width)
+		{
+			write(1, " ", tab->width);
+			write(1, &ch, 1);
+		}
+		return (tab->width + 1);
 	}
 	return (0);
 }
