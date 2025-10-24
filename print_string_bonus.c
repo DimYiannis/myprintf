3bbc5a9797e77d24f6a5ed67ccf471a19d79485e
DimYiannis <johndim99@gmail.com>
2025-10-24 22:10:04 +0300

    used the struct to access the next arg via the va_arg macro

--- print_string_bonus.c
+++ print_string_bonus.c
@@ -6,21 +6,32 @@
 /*   By: ydimitra <ydimitra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/24 09:54:49 by ydimitra          #+#    #+#             */
-/*   Updated: 2025/10/24 11:19:19 by ydimitra         ###   ########.fr       */
+/*   Updated: 2025/10/24 22:07:29 by ydimitra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	string_case(va_list *args, char c)
+int	string_case(t_print *tab)
 {
-	char	*tmp;
+	int len;
+	char *s;
 
-	if (c == 's')
+	s = va_arg(tab->args, char *);
+	if (!s)
+		return (write(1, "(null)", 6));
+	len = ft_strlen(s);
+	if (tab->dash && tab->width)
 	{
-		tmp = va_arg(*args, char *);
-		if (!tmp)
-			tmp = "(null)";
-		return (write(1, tmp, ft_strlen(tmp)));
+		write(1, s, len);
+		len += write(1, " ", tab->width);
 	}
+	else if (tab->width)
+	{
+		len += write(1, " ", tab->width);
+		write(1, s, len - tab->width);
+	}
+	if (tab->precision)
+		len += write(1, s, tab->precision);
+	return (len);
 }
