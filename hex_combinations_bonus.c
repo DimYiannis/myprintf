a1475caf6513eea1f85d46d795e52d8be2741ced
DimYiannis <johndim99@gmail.com>
2025-10-25 19:00:18 +0200

    precision

--- hex_combinations_bonus.c
+++ hex_combinations_bonus.c
@@ -6,38 +6,15 @@
 /*   By: ydimitra <ydimitra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/25 15:46:41 by ydimitra          #+#    #+#             */
-/*   Updated: 2025/10/25 18:34:55 by ydimitra         ###   ########.fr       */
+/*   Updated: 2025/10/25 18:55:59 by ydimitra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf_bonus.h"
 
-int	hex_width(unsigned int n, char c, t_print *tab)
-{
-	if (tab->width)
-	{
-		if (len < tab->width)
-		{
-			len = putnchar(' ', tab->width - len);
-			if (c == 'x')
-				len = ft_hexputnbr(n, 0);
-			else
-				len = ft_hexputnbr(n, 1);
-		}
-		else
-		{
-			if (c == 'x')
-				len = ft_hexputnbr(n, 0);
-			else
-				len = ft_hexputnbr(n, 1);
-		}
-	}
-	return (len);
-}
-
 int	hex_hash_width(unsigned int n, char c, t_print *tab)
 {
-	if (tab->width)
+	if (tab->hash)
 	{
 		if (len < tab->width)
 		{
@@ -46,6 +23,16 @@ int	hex_hash_width(unsigned int n, char c, t_print *tab)
 		}
 		else
 			len = hashflag(n, c);
+	}
+	else
+	{
+		if (len < tab->width)
+		{
+			len = putnchar(' ', tab->width - len);
+			len += no_hashflag(n, c);
+		}
+		else
+			len = no_hashflag(n, c);
 	}
 	return (len);
 }
@@ -79,7 +66,7 @@ int	hex_dash_width_prec(unsigned int n, char c, t_print *tab)
 
 int	hex_width_zero(char *tmp, int len, char c)
 {
-	if (tab->width && tab->zero)
+	if (tab->width)
 	{
 		if (len < tb->width)
 		{
@@ -88,6 +75,43 @@ int	hex_width_zero(char *tmp, int len, char c)
 		}
 		else
 			len = no_hashflag(n, c);
+	}
+	else if (tab->width && tab->hash)
+	{
+		if (len < tb->width)
+		{
+			len = putnchar('0', tab->width - len);
+			len += hashflag(n, c);
+		}
+		else
+			len = hashflag(n, c);
+	}
+	return (len);
+}
+
+int hex_precision(char *tmp, int len, char c)
+{
+	if (tab->hash)
+	{
+		len = putnchar('0', tab->width - tab->precision);
+		if (len < tab->precision)
+		{
+			len += putnchar('0', tab->precision - len);
+			len += hashflag(tmp, len);
+		}
+		else
+			len += hashflag(tmp, len);
+	}
+	else
+	{
+		len = putnchar('0', tab->width - tab->precision);
+		if (len < tab->precision)
+		{
+			len += putnchar('0', tab->precision - len);
+			len += no_hashflag(tmp, len);
+		}
+		else
+			len += no_hashflag(tmp, len);
 	}
 	return (len);
 }
