dff97948dd7502afb9a2ac2c72c74306874e8bd0
DimYiannis <johndim99@gmail.com>
2025-10-25 16:30:44 +0200

    checking cases

--- print_hex_bonus.c
+++ print_hex_bonus.c
@@ -6,7 +6,7 @@
 /*   By: ydimitra <ydimitra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/24 09:54:41 by ydimitra          #+#    #+#             */
-/*   Updated: 2025/10/24 14:31:09 by ydimitra         ###   ########.fr       */
+/*   Updated: 2025/10/25 16:30:08 by ydimitra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,26 +28,18 @@ unsigned long	ft_hexputnbr(unsigned long n, int upper)
 	return (len);
 }
 
-unsigned long	hex_case(va_list *args, const char *c)
+unsigned long	hex_case(args, const char *c)
 {
+	unsigned long	len;
+
+	len = 0;
 	if (*c == 'x' || *c == 'X')
 	{
 		u = va_arg(*args, unsigned int);
-		if (flag == '#' &&u != 0)
-		{
-			if (*c == 'x')
-			{
-				len += write(1, "0x", 2);
-				return (len + ft_hexputnbr(u, 0));
-			}
-				
-			else
-			{
-				len += write(1, "0X", 2);
-				return (len + ft_hexputnbr(u, 1));
-			}	
-		}	
-		return (len + ft_hexputnbr(u, (*c == 'X')));
+		if (tab->hash && u != 0)
+			hashflag(u, *c);
+		else if (tab->width && tab->hash)
+			hex_hash_width(u, *c)
 	}
-	return (0);
+	return (len);
 }
