46b047226f27daff8219e563b6b893687d2613d1
DimYiannis <johndim99@gmail.com>
2025-10-25 15:32:49 +0200

    possible finished used funcs in this one

--- print_uint_bonus.c
+++ print_uint_bonus.c
@@ -6,7 +6,7 @@
 /*   By: ydimitra <ydimitra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/24 09:54:52 by ydimitra          #+#    #+#             */
-/*   Updated: 2025/10/24 09:58:23 by ydimitra         ###   ########.fr       */
+/*   Updated: 2025/10/25 15:30:32 by ydimitra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,14 @@ static int	unsigned_case(va_list *args, char c)
 	{
 		tmp = ft_unsigneditoa(va_arg(*args, unsigned int));
 		len = ft_strlen(tmp);
+		if (tab->width)
+			len = width_prec(tmp, len);
+		else if (tab->width && tab->zero)
+			len = width_zero(tmp, len);
+		else if (tab->dash)
+			len = dash_width_prec(tmp, len);
+		else if (tab->dash && tab->precision)
+			len += dash_prec(tmp, len);
 		write(1, tmp, len);
 		free(tmp);
 		return (len);
