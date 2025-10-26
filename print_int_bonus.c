7395a0a45b5438b43286a2472e6c7523d8182d3b
DimYiannis <johndim99@gmail.com>
2025-10-26 14:16:58 +0100

    compiles

--- print_int_bonus.c
+++ print_int_bonus.c
@@ -6,49 +6,48 @@
 /*   By: ydimitra <ydimitra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/24 09:54:44 by ydimitra          #+#    #+#             */
-/*   Updated: 2025/10/25 17:43:29 by ydimitra         ###   ########.fr       */
+/*   Updated: 2025/10/26 14:16:16 by ydimitra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_printf.h"
+#include "ft_printf_bonus.h"
 
 int	int_case(t_print *tab, char c)
 {
 	char	*tmp;
 	int		len;
-	char	ch;
 	int		num;
 
-    if (c == "i" || c == "d")
+	num = va_arg(tab->args, int);
+	tmp = ft_itoa(num);
+	if (!tmp)
+		return (0);
+	len = ft_strlen(tmp);
+	if (c == 'i' || c == 'd')
 	{
-		num = va_arg(tab->args, int);
-		tmp = ft_itoa(num);
-		if (!tmp)
-			return (0);
-		len = ft_strlen(tmp);
 		if (tab->width)
 		{
 			if (len < tab->width)
 			{
 				if (tab->precision)
 				{
-					len = putnchar('', tab->width - tab->precision);
+					len = putnchar(' ', tab->width - tab->precision);
 					if (len < tab->precision)
 					{
 						len += putnchar(' ', tab->precision - len);
-						len += putnchar(tmp, len);
+						len += putnchar(*tmp, len);
 					}
 					else
-						len += putnchar(tmp, len);
+						len += putnchar(*tmp, len);
 				}
 				else
 				{
 					len = putnchar(' ', tab->width - len);
-					len += putnchar(tmp, len);
+					len += putnchar(*tmp, len);
 				}
 			}
 			else
-				len = putnchar(tmp, len);
+				len = putnchar(*tmp, len);
 		}
 		else if (tab->width && tab->sign)
 		{
@@ -57,10 +56,10 @@ int	int_case(t_print *tab, char c)
 				if (len < tab->width)
 				{
 					len = putnchar(' ', tab->width - len);
-					len += putnchar(tmp, len);
+					len += putnchar(*tmp, len);
 				}
 				else
-					len = putnchar(tmp, len);
+					len = putnchar(*tmp, len);
 			}
 			else
 			{
@@ -68,49 +67,49 @@ int	int_case(t_print *tab, char c)
 				{
 					len = putnchar(' ', tab->width - len - 1);
 					len += write(1, "+", 1);
-					len += putnchar(tmp, len);
+					len += putnchar(*tmp, len);
 				}
 				else
-					len = putnchar(tmp, len);
+					len = putnchar(*tmp, len);
 			}
 		}
 		else if (tab->sp)
 		{
 			len = write(1, " ", 1);
-			len += putnchar(tmp, len);
+			len += putnchar(*tmp, len);
 			if (tab->width)
 			{
 				if (len > tab->width)
-					len = putnchar(tmp, len);
+					len = putnchar(*tmp, len);
 				else
 				{
 					len = putnchar(' ', tab->width - len);
-					len += putnchar(tmp, len)
+					len += putnchar(*tmp, len);
 				}
 			}
 		}
 		else if (tab->width && tab->zero)
-    	{
-        	len = putnchar('0', tab->width - len);
-			len += putnchar(1, tmp, len);
-    	}
+		{
+			len = putnchar('0', tab->width - len);
+			len += putnchar(*tmp, len);
+		}
 		else if (tab->dash)
 		{
 			if (tab->width)
 			{
 				if (len < tab->width)
 				{
-					putnchar(tmp, len);
+					putnchar(*tmp, len);
 					len += putnchar(' ', tab->width - len);
 				}
 				else
-					putnchar(tmp, len);
+					putnchar(*tmp, len);
 			}
 			else
-				putnchar(tmp, len);
+				putnchar(*tmp, len);
 		}
-		putnchar(tmp, len);
-		free(tmp);
-		return (len);
 	}
+	putnchar(*tmp, len);
+	free(tmp);
+	return (len);
 }
