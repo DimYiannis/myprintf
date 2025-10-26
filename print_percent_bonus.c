eb70dd505cbfd4735d73ea94c1b292ef6a6ac1f6
DimYiannis <johndim99@gmail.com>
2025-10-26 13:15:52 +0100

    finished percent

--- print_percent_bonus.c
+++ print_percent_bonus.c
@@ -6,7 +6,7 @@
 /*   By: ydimitra <ydimitra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/24 22:14:36 by ydimitra          #+#    #+#             */
-/*   Updated: 2025/10/24 23:07:46 by ydimitra         ###   ########.fr       */
+/*   Updated: 2025/10/26 13:15:12 by ydimitra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,17 +29,19 @@ int	percent_case(t_print *tab, char c)
 	if (tab->dash && tab->width)
 	{
 		write(1, &c, 1);
-		write(1, " ", tab->width - 1);
+		putnchar(1, ' ', tab->width - 1);
 	}
-	else if (tab->width)
-	{
-		write(1, " ", tab->width - 1);
-		write(1, &c, 1);
-	}
-    else if (tab->width && tab->zero)
+	else if (tab->width && tab->zero)
     {
-        write(1, "0", tab->width - 1);
+        putnchar(1, '0', tab->width - 1);
 		write(1, &c, 1);
     }
+	else if (tab->width)
+	{
+		putnchar(1, ' ', tab->width - 1);
+		write(1, &c, 1);
+	}
+	else
+		write(1, &c, 1);
 	return (tab->width + 1);
 }
