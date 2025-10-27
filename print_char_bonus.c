9c1735ca38cf58adf8a5a7d2db71a06193b8982d
DimYiannis <johndim99@gmail.com>
2025-10-27 14:21:15 +0100

    i didnt use the next arg before

--- print_char_bonus.c
+++ print_char_bonus.c
@@ -6,30 +6,26 @@
 /*   By: ydimitra <ydimitra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/24 09:55:33 by ydimitra          #+#    #+#             */
-/*   Updated: 2025/10/26 22:04:12 by ydimitra         ###   ########.fr       */
+/*   Updated: 2025/10/27 14:20:13 by ydimitra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf_bonus.h"
 
-int	char_case(t_print *tab, char c)
+int	char_case(t_print *tab)
 {
 	int padding;
+	char c;
 	
 	padding = 0;
+	c = (char)va_arg(tab->args, int);
 	if (tab->width > 1)
 		padding = tab->width - 1;
+	if (!tab->dash)
+		tab->total_length += putchar_n(' ', padding);
+	
+	tab->total_length += write(1, &c, 1);
 	if (tab->dash)
-	{
-		write(1, &c, 1);
-		putchar_n(' ', padding);
-	}
-	else
-	{
-		putchar_n(' ', padding);
-		write(1, &c, 1);
-	}
-	if (tab->width > 1)
-		return (tab->width + 1);
-	return (1);
+		tab->total_length += putchar_n(' ', padding);
+	return (tab->total_length);
 }
