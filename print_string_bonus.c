9a7ebf4b862ea0cbda5e65aa607bef5766b9aa63
DimYiannis <johndim99@gmail.com>
2025-10-27 14:39:32 +0100

    fixd logic of string

--- print_string_bonus.c
+++ print_string_bonus.c
@@ -6,7 +6,7 @@
 /*   By: ydimitra <ydimitra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/24 09:54:49 by ydimitra          #+#    #+#             */
-/*   Updated: 2025/10/26 22:08:42 by ydimitra         ###   ########.fr       */
+/*   Updated: 2025/10/27 14:38:21 by ydimitra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,37 +16,20 @@ int	string_case(t_print *tab)
 {
 	int len;
 	char *s;
+	int padding;
 
 	s = va_arg(tab->args, char *);
 	if (!s)
 		return (write(1, "(null)", 6));
 	len = ft_strlen(s);
-	if (tab->dash && tab->width)
-	{
-		if (tab->precision)
-			len = putstring_n(s, tab->precision);
-		else
-			len = putstring_n(s, len);
-		len += putchar_n(' ', tab->width - len);
-	}
-	else if (tab->width)
-	{
-		if (len < tab->width)
-		{
-			if (tab->precision)
-			{
-				len = putchar_n(' ', tab->width - tab->precision);
-				len += putstring_n(s, tab->precision);
-			}
-			else
-			{
-				len = putchar_n(' ',tab->width - len);
-				len += putstring_n(s, len);
-			}	
-			return (len);
-		}
-		else
-			putstring_n(s, len);
-	}
-	return (len);
+	if (tab->precision && tab->precision < len)
+		len = tab->precision;
+	if (tab->width > len)
+		padding = tab->width - len;
+	if (!tab->dash)
+		tab->total_length += putchar_n('0', padding);
+	tab->total_length += putstring_n(s, len);
+	if (tab->dash)
+		tab->total_length += putchar_n('0', padding);
+	return (tab->total_length);
 }
