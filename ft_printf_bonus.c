62f30d1986b299b0a8f17ccf95b0e008b232f6cd
DimYiannis <johndim99@gmail.com>
2025-10-27 18:14:26 +0100

    changes in struct

--- ft_printf_bonus.c
+++ ft_printf_bonus.c
@@ -6,7 +6,7 @@
 /*   By: ydimitra <ydimitra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/24 11:16:10 by ydimitra          #+#    #+#             */
-/*   Updated: 2025/10/27 11:13:35 by ydimitra         ###   ########.fr       */
+/*   Updated: 2025/10/27 17:26:52 by ydimitra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,12 @@ static t_print	*tab_initialise(t_print *tab)
 	tab->width = 0;
 	tab->precision = 0;
 	tab->zero = 0;
-	tab->pnt = 0;
 	tab->dash = 0;
 	tab->total_length = 0;
 	tab->sign = 0;
-	tab->is_zero = 0;
-	tab->perc = 0;
 	tab->sp = 0;
 	tab->hash = 0;
+	tab->pnt_zero = 0;
 	return (tab);
 }
 int	ft_printf(const char *type, ...)
