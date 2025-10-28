4c8d1972ee8aea6730c0e665d90d1f2457c5ec1d
DimYiannis <johndim99@gmail.com>
2025-10-29 00:06:24 +0200

    two helper funcs to improve the file that prints uint

--- uint_combinations_bonus.c
+++ uint_combinations_bonus.c
@@ -6,117 +6,47 @@
 /*   By: ydimitra <ydimitra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/25 15:14:46 by ydimitra          #+#    #+#             */
-/*   Updated: 2025/10/27 10:13:32 by ydimitra         ###   ########.fr       */
+/*   Updated: 2025/10/28 23:32:46 by ydimitra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf_bonus.h"
 
-int	width_prec(t_print *tab, char *tmp, int len)
+int	count_decimal_digits(unsigned int n)
 {
-	if (tab->width)
+	int count;
+	
+	if (n == 0)
+		return (1);
+	count = 0;
+	while (n > 0)
 	{
-		if (len < tab->width)
-		{
-			if (tab->precision)
-			{
-				len = putchar_n(' ', tab->width - tab->precision);
-				if (len < tab->precision)
-				{
-					len += putchar_n('0', tab->precision - len);
-					len += putstring_n(tmp, len);
-				}
-				else
-					len += putstring_n(tmp, len);
-			}
-			else
-			{
-				len = putchar_n(' ', tab->width - len);
-				len += putstring_n(tmp, len);
-			}
-		}
-		else
-			len += putstring_n(tmp, len);
+		n /= 10;
+		count++;
 	}
-	return (len);
+	return (count);
 }
 
-int	dash_width_prec(t_print *tab, char *tmp, int len)
+char	*ft_unsigneditoa(unsigned int n)
 {
-	if (tab->dash)
-	{
-		if (tab->width)
-		{
-			if (len < tab->width)
-			{
-				putstring_n(tmp, len);
-				len += putchar_n(' ', tab->width - len);
-			}
-			else if (tab->precision)
-			{
-				if (len < tab->precision)
-				{
-					len += putchar_n('0', tab->precision - len);
-					len += putstring_n(tmp, len);
-					if (tab->width > tab->precision)
-						len += putchar_n(' ', tab->width - tab->precision);
-				}
-				else
-					len += putstring_n(tmp, len);
-			}
-			else
-				len = putstring_n(tmp, len);
-		}
-	}
-	return (len);
-}
+	char			*s;
+	unsigned int	len;
 
-int	prec(t_print *tab, char *tmp, int len)
-{
-	if (tab->precision)
+	len = count_decimal_digits(n);
+	s = malloc(len + 1);
+	if (!s)
+		return (NULL);
+	s[len] = '\0';	
+	if (n == 0)
 	{
-		len = putchar_n('0', tab->width - tab->precision);
-		if (len < tab->precision)
-		{
-			len += putchar_n('0', tab->precision - len);
-			len += putstring_n(tmp, len);
-		}
-		else
-			len += putstring_n(tmp, len);
+		s[0] = '0';
+		return (s);
 	}
-	return (len);
-}
-
-int	width_zero(t_print *tab, char *tmp, int len)
-{
-	if (tab->width && tab->zero)
+	while (len > 0)
 	{
-		if (len < tab->width)
-		{
-			len = putchar_n('0', tab->width - len);
-			len += putstring_n(tmp, len);
-		}
-		else
-			len = putstring_n(tmp, len);
+		len--;
+		s[len] = (n % 10) + '0';
+		n /= 10;
 	}
-	return (len);
-}
-int	dash_prec(t_print *tab, char *tmp, int len)
-{
-	if (tab->dash)
-	{
-		if (tab->precision)
-		{
-			if (len < tab->precision)
-			{
-				len += putchar_n('0', tab->precision - len);
-				len += putstring_n(tmp, len);
-			}
-			else
-				len += putstring_n(tmp, len);
-		}
-		else
-			len = putstring_n(tmp, len);
-	}
-	return (len);
+	return (s);
 }
