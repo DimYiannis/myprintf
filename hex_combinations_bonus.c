fb0e1847f770d9d444824cef146a487fad326b58
DimYiannis <johndim99@gmail.com>
2025-10-28 19:03:37 +0200

    new funcs for handling properly prefix and padding

--- hex_combinations_bonus.c
+++ hex_combinations_bonus.c
@@ -5,217 +5,60 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: ydimitra <ydimitra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/10/25 15:46:41 by ydimitra          #+#    #+#             */
-/*   Updated: 2025/10/27 10:08:16 by ydimitra         ###   ########.fr       */
+/*   Created: 2025/10/24 09:54:41 by ydimitra          #+#    #+#             */
+/*   Updated: 2025/10/28 18:51:04 by ydimitra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf_bonus.h"
 
-int	hex_hash_width(t_print *tab, unsigned int n, char c)
+int	print_prefix(unsigned int num, int upper, int hash)
 {
-	int len;
-
-	len = hex_len(n);
-	if (tab->hash)
-	{
-		if (len < tab->width)
-		{
-			len = putchar_n(' ', tab->width - len);
-			len += hashflag(tab, n, c);
-		}
-		else
-			len = hashflag(tab, n, c);
-	}
-	else
-	{
-		if (len < tab->width)
-		{
-			len = putchar_n(' ', tab->width - len);
-			len += no_hashflag(tab, n, c);
-		}
-		else
-			len = no_hashflag(tab, n, c);
-	}
-	return (len);
+	if (!hash || num == 0)
+		return (0);
+	if (upper)
+		return (write(1, "0X", 2));
+	return (write(1, "0x", 2));
 }
 
-int	hex_dash_width_prec(t_print *tab, unsigned int n, char c)
+int	print_main(char *tmp, int len, int zeros)
 {
-	int len;
-	
-	len = hex_len(n);
-	if (tab->width)
-	{
-		if (len < tab->width)
-		{
-			len = no_hashflag(tab, n, c);
-			len += putchar_n(' ', tab->width - len);
-		}
-		else if (tab->precision)
-		{
-			if (len < tab->precision)
-			{
-				len = putchar_n('0', tab->precision - len);
-				len += no_hashflag(tab, n, c);
-				if (tab->width > tab->precision)
-					len += putchar_n(' ', tab->width - tab->precision);
-			}
-			else
-				len = no_hashflag(tab, n, c);
-		}
-		else
-			len = no_hashflag(tab, n, c);
-	}
-	return (len);
+	int written;
+
+	written = 0;
+	written += putchar_n('0', zeros);
+	written += putstring_n(tmp, len);
+	return (written);
 }
 
-int	hex_width_zero(t_print *tab, unsigned int n, char c)
+int	print_hex_left(char *tmp, unsigned int num, t_hexinfo info)
 {
-	int len;
-	
-	len = hex_len(n);
-	if (tab->width)
-	{
-		if (len < tab->width)
-		{
-			len = putchar_n('0', tab->width - len);
-			len += no_hashflag(tab, n, c);
-		}
-		else
-			len = no_hashflag(tab, n, c);
-	}
-	else if (tab->width && tab->hash)
-	{
-		if (len < tab->width)
-		{
-			len = putchar_n('0', tab->width - len);
-			len += hashflag(tab, n, c);
-		}
-		else
-			len = hashflag(tab, n, c);
-	}
-	return (len);
+	int written;
+
+	written = 0;
+	written += print_prefix(num, info.upper, info.prefix);
+	written += print_main(tmp, info.len, info.zeros);
+	written += putchar_n(' ', info.padding);
+	return (written);
 }
 
-int hex_precision(t_print *tab,  unsigned int n, char c)
+int	print_hex_right(t_print *tab, char *tmp, unsigned int num, t_hexinfo info)
 {
-	int len; 
-	
-	hex_len(n);
-	if (tab->hash)
+	int written;
+
+	written = 0;
+	if (!tab->zero || tab->precision >= 0)
+		written += putchar_n(' ', info.padding);
+	if (tab->zero && tab->precision < 0)
 	{
-		len = putchar_n('0', tab->width - tab->precision);
-		if (len < tab->precision)
-		{
-			len += putchar_n('0', tab->precision - len);
-			len += hashflag(tab, len, c);
-		}
-		else
-			len += hashflag(tab, len, c);
+		if (info.prefix)
+			written += print_prefix(num, info.upper, info.prefix);
+		written += putchar_n('0', info.padding);
 	}
-	else
-	{
-		len = putchar_n('0', tab->width - tab->precision);
-		if (len < tab->precision)
-		{
-			len += putchar_n('0', tab->precision - len);
-			len += no_hashflag(tab, len, c);
-		}
-		else
-			len += no_hashflag(tab, len, c);
-	}
-	return (len);
+	if (!tab->zero || tab->precision >= 0)
+		written += print_prefix(num, info.upper, info.prefix);
+	written += print_main(tmp, info.len, info.zeros);
+	return (written);
 }
 
-int	hex_width_prec(t_print *tab,  unsigned int n, const char c)
-{
-	int len;
 
-	len = hex_len(n);
-	if (tab->hash)
-	{
-		if (len < tab->width)
-		{
-			len = putchar_n(' ', tab->width - tab->precision);
-			if (len < tab->precision)
-			{
-				len += putchar_n('0', tab->precision - len);
-				len += hashflag(tab, n, c);
-			}
-			else
-				len += hashflag(tab, n, c);
-		}
-		else
-			len = hashflag(tab, n, c);
-	}
-	else
-	{
-		if (len < tab->width)
-		{
-			len = putchar_n(' ', tab->width - tab->precision);
-			if (len < tab->precision)
-			{
-				len += putchar_n('0', tab->precision - len);
-				len += no_hashflag(tab, n, c);
-			}
-			else
-				len += no_hashflag(tab, n, c);		
-		}
-		else
-			len = no_hashflag(tab, n, c);
-	}
-	return (len);
-}
-
-int	hash_dash_width_prec(t_print *tab, unsigned int n, char c)
-{
-	int len;
-	
-	len = hex_len(n);
-	if (tab->hash)
-	{
-		if (len < tab->width)
-		{
-			len = hashflag(tab, n, c);
-			len += putchar_n(' ', tab->width - len);
-		}
-		else if (tab->precision)
-		{
-			if (len < tab->precision)
-			{
-				len = putchar_n('0', tab->precision - len);
-				len += hashflag(tab, n, c);
-				if (tab->width > tab->precision)
-					len += putchar_n(' ', tab->width - tab->precision);
-			}
-			else
-				len = hashflag(tab, n, c);
-		}
-		else
-			len = hashflag(tab, n, c);
-	}
-	else
-	{
-		if (len < tab->width)
-		{
-			len += no_hashflag(tab, n, c);
-			len += putchar_n(' ', tab->width - len);
-		}
-		else if (tab->precision)
-		{
-			if (len < tab->precision)
-			{
-				len += putchar_n('0', tab->precision - len);
-				len += no_hashflag(tab, n, c);
-				if (tab->width > tab->precision)
-					len += putchar_n(' ', tab->width - tab->precision);
-			}
-			else
-				len += no_hashflag(tab, n, c);
-		}
-		else
-			len += no_hashflag(tab, n, c);
-	}
-	return (len);
-}
