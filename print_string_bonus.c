fd7b065981aeee7274d01a5925d624b404af49c5
DimYiannis <johndim99@gmail.com>
2025-10-28 22:26:52 +0200

    splitted the func for norm

--- print_string_bonus.c
+++ print_string_bonus.c
@@ -6,37 +6,42 @@
 /*   By: ydimitra <ydimitra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/24 09:54:49 by ydimitra          #+#    #+#             */
-/*   Updated: 2025/10/27 22:24:40 by ydimitra         ###   ########.fr       */
+/*   Updated: 2025/10/28 22:19:59 by ydimitra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf_bonus.h"
 
+static int compute_string(char **s, t_print *tab)
+{
+	int len;
+
+	*s = va_arg(tab->args, char *);
+	if (!*s)
+	{
+		if (tab->precision >= 0 && tab->precision < 6)
+			*s = "";
+		else
+			*s = "(null)";
+	}
+	len = ft_strlen(*s);
+	if (tab->precision >= 0 && tab->precision < len)
+		len = tab->precision;
+	return (len);
+}
+
 int	string_case(t_print *tab)
 {
 	int len;
-	char *s;
 	int padding;
+	char *s;
 
-	s = va_arg(tab->args, char *);
-	if (!s)
-	{
-    	if (tab->precision >= 0 && tab->precision < 6)
-    	{
-        	len = 0;
-        	s = "";
-    	}
-    	else
-        s = "(null)";
-	}	
-	len = ft_strlen(s);
-	if (tab->precision >= 0 && tab->precision < len)
-		len = tab->precision;
+	len = compute_string(&s, tab);
 	padding = 0;
-	 if (tab->width > len)
-        padding = tab->width - len;
+	if (tab->width > len)
+		padding = tab->width - len;
 	if (!tab->dash && padding > 0)
-		tab->total_length += putchar_n(' ', padding);
+        tab->total_length += putchar_n(' ', padding);
 	tab->total_length += putstring_n(s, len);
 	if (tab->dash && padding > 0)
 		tab->total_length += putchar_n(' ', padding);
