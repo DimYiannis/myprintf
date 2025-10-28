c08855e0dac19f63c15e2561cc1f04abec48ea39
DimYiannis <johndim99@gmail.com>
2025-10-28 13:15:25 +0200

    counting logic

--- print_percent_bonus.c
+++ print_percent_bonus.c
@@ -6,7 +6,7 @@
 /*   By: ydimitra <ydimitra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/24 22:14:36 by ydimitra          #+#    #+#             */
-/*   Updated: 2025/10/26 22:04:30 by ydimitra         ###   ########.fr       */
+/*   Updated: 2025/10/28 12:57:22 by ydimitra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,22 +26,10 @@
 
 int	percent_case(t_print *tab, char c)
 {
-	if (tab->dash && tab->width)
-	{
-		write(1, &c, 1);
-		putchar_n(' ', tab->width - 1);
-	}
-	else if (tab->width && tab->zero)
-    {
-        putchar_n('0', tab->width - 1);
-		write(1, &c, 1);
-    }
-	else if (tab->width)
-	{
-		putchar_n(' ', tab->width - 1);
-		write(1, &c, 1);
-	}
-	else
-		write(1, &c, 1);
+	if (tab->dash && tab->width > 1)
+		tab->total_length += putchar_n(' ', tab->width - 1);
+	tab->total_length += write(1,&c, 1);
+	if (tab->dash && tab->width > 1)
+	tab->total_length += putchar_n(' ', tab->width - 1);
 	return (tab->width + 1);
 }
