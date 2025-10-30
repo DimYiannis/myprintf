c9a8ed074bcb03f427d64250941a54076825acd5
DimYiannis <johndim99@gmail.com>
2025-10-30 12:47:47 +0100

    used a pointer for the second struct and not a struct pointer so i needed to change the way i was accessing the vars

--- hex_helpers_bonus.c
+++ hex_helpers_bonus.c
@@ -6,7 +6,7 @@
 /*   By: ydimitra <ydimitra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/28 23:58:58 by ydimitra          #+#    #+#             */
-/*   Updated: 2025/10/29 15:10:06 by ydimitra         ###   ########.fr       */
+/*   Updated: 2025/10/30 12:42:41 by ydimitra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,9 +36,9 @@ int	print_hex_left(char *tmp, unsigned int num, t_hexinfo info)
 	int	written;
 
 	written = 0;
-	written += print_prefix(num, info->upper, info->prefix);
-	written += print_main(tmp, info->len, info->zeros);
-	written += putchar_n(' ', info->padding);
+	written += print_prefix(num, info.upper, info.prefix);
+	written += print_main(tmp, info.len, info.zeros);
+	written += putchar_n(' ', info.padding);
 	return (written);
 }
 
@@ -48,15 +48,15 @@ int	print_hex_right(t_print *tab, char *tmp, unsigned int num, t_hexinfo info)
 
 	written = 0;
 	if (!tab->zero || tab->precision >= 0)
-		written += putchar_n(' ', info->padding);
+		written += putchar_n(' ', info.padding);
 	if (tab->zero && tab->precision < 0)
 	{
-		if (info->prefix)
-			written += print_prefix(num, info->upper, info->prefix);
-		written += putchar_n('0', info->padding);
+		if (info.prefix)
+			written += print_prefix(num, info.upper, info.prefix);
+		written += putchar_n('0', info.padding);
 	}
 	if (!tab->zero || tab->precision >= 0)
-		written += print_prefix(num, info->upper, info->prefix);
-	written += print_main(tmp, info->len, info->zeros);
+		written += print_prefix(num, info.upper, info.prefix);
+	written += print_main(tmp, info.len, info.zeros);
 	return (written);
 }
